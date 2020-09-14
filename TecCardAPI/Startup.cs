617799efede4af3c9ef91a680d3d9c62feb99606
Vinicius-Ricci <viniricci02@gmail.com>
2020-09-14 08:20:09 -0300

    Organizando projeto

--- TecCardAPI/Startup.cs
+++ TecCardAPI/Startup.cs
@@ -39,7 +39,7 @@ namespace TecCardAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
